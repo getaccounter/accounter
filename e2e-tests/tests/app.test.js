2daf9f53c5aebb511fa90f84266a34fc112554aa
Mo Sattler <mail@mosattler.com>
2020-10-04 21:10:52 +0200

    add screenshots to ignore file

--- e2e-tests/tests/app.test.js
+++ e2e-tests/tests/app.test.js
@@ -2,9 +2,5 @@ describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
     cy.findByText("Learn React").should("exist");
-    cy.visit("/api/");
-    cy.findByText("The install worked successfully! Congratulations!").should(
-      "exist"
-    );
   });
 });
