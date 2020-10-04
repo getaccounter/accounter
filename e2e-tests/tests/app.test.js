b664354692a3ad0c43dfbdb1ed1a55b0eee3d4c1
Mo Sattler <mail@mosattler.com>
2020-10-04 20:59:24 +0200

    Add tests for api route in nginx

--- e2e-tests/tests/app.test.js
+++ e2e-tests/tests/app.test.js
@@ -1,6 +1,10 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/') 
-    cy.findByText('Learn React').should('exist')
-  })
-})
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("/");
+    cy.findByText("Learn React").should("exist");
+    cy.visit("/api/");
+    cy.findByText("The install worked successfully! Congratulations!").should(
+      "exist"
+    );
+  });
+});
