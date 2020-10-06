09fd5c690ee14becaef242da8a3ec176769f01f2
Mo Sattler <hi@mosattler.com>
2020-10-06 13:03:57 +0200

    add e2e tests for django

--- e2e-tests/tests/app.test.js
+++ e2e-tests/tests/app.test.js
@@ -1,6 +1,12 @@
 describe("The Home Page", () => {
-  it("successfully loads", () => {
+  it("successfully loads web", () => {
     cy.visit("/");
     cy.findByText("Learn React").should("exist");
+  });
+  it("successfully loads server", () => {
+    cy.visit("/api/");
+    cy.findByText("The install worked successfully! Congratulations!").should(
+      "exist"
+    );
   });
 });
