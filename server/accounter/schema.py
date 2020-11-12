18acd2c1a6626e4d58085102524550e5371abec4
Mo Sattler <hi@mosattler.com>
2020-11-12 20:58:14 +0100

    add isort

--- server/accounter/schema.py
+++ server/accounter/schema.py
@@ -1,7 +1,6 @@
 import graphene
-from graphene_django import DjangoObjectType
-
 from django.contrib.auth.models import User
+from graphene_django import DjangoObjectType
 
 
 class UserType(DjangoObjectType):
--- server/accounter/urls.py
+++ server/accounter/urls.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.urls import path
 from graphene_django.views import GraphQLView
-from django.conf import settings
-
 
 urlpatterns = [
     path("graphql", GraphQLView.as_view(graphiql=settings.DEBUG)),
