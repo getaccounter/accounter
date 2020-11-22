d647f960b61657a1ca35a8106f9f1413e7d1b8b0
Mo Sattler <hi@mosattler.com>
2020-11-22 12:55:28 +0100

    Update tailwindcss

--- e2e-tests/docker-entrypoint.sh
+++ e2e-tests/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-./wait-for-it.sh -s $LOADBALANCER_HOST:$LOADBALANCER_PORT -- echo "App is ready"
+./wait-for-it.sh -s $LOADBALANCER_HOST:$LOADBALANCER_PORT -t 30 -- echo "App is ready"
 
 exec "$@"
