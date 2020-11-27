3b0f2aed3535ec222c44e9e0381927857079c8b6
Mo Sattler <hi@mosattler.com>
2020-11-27 17:01:18 +0100

    improve data model

--- server/accounter/integrations/models.py
+++ server/accounter/integrations/models.py
@@ -1,12 +1,36 @@
-from django.conf import settings
 from django.db import models
+
+from ..organizations.models import Organization, Profile
 
 
 class Service(models.Model):
-    name = models.CharField(max_length=16)
+    class Types(models.TextChoices):
+        SLACK = "SLACK", "Slack"
+
+    name = models.CharField("Type", max_length=50, choices=Types.choices, unique=True)
     logo = models.FileField(upload_to="services/logos")
 
 
+# Abstract Classes
 class Integration(models.Model):
-    # Add timestamps
-    service = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT)
+    organization = models.ForeignKey(Organization, on_delete=models.RESTRICT)
+
+    class Meta:
+        abstract = True
+
+
+class Account(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.RESTRICT)
+
+    class Meta:
+        abstract = True
+
+
+# Slack
+class SlackIntegration(Integration):
+    pass
+
+
+class SlackAccount(Account):
+    integration = models.ForeignKey(SlackIntegration, on_delete=models.RESTRICT)
+    pass
--- server/accounter/organizations/models.py
+++ server/accounter/organizations/models.py
@@ -1,12 +1,9 @@
 from django.conf import settings
 from django.db import models
 
-from ..integrations.models import Integration
-
 
 class Organization(models.Model):
     name = models.CharField(max_length=100)
-    integrations = models.ManyToManyField(Integration)
 
 
 class Admin(models.Model):
@@ -16,8 +13,3 @@ class Admin(models.Model):
 
 class Profile(models.Model):
     organization = models.ForeignKey(Organization, on_delete=models.RESTRICT)
-
-
-class Account(models.Model):
-    integration = models.ForeignKey(Integration, on_delete=models.RESTRICT)
-    profile = models.ForeignKey(Profile, on_delete=models.RESTRICT)
