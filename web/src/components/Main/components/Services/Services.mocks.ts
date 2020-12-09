93383c3b586cbac8bd6644ba7b14923f683ae24c
Mo Sattler <hi@mosattler.com>
2020-12-09 20:49:58 +0100

    fix services

--- web/src/components/Main/components/Services/Services.mocks.ts
+++ web/src/components/Main/components/Services/Services.mocks.ts
@@ -2,7 +2,6 @@ import { GET_SERVICE_LIST_QUERY, Service } from "./components/AddMoreServices";
 import * as Factory from "factory";
 
 export const serviceFactory = Factory.Sync.makeFactory<Service>({
-  id: Factory.each((i) => i),
   name: "service",
   logo: "path/to/logo",
   oauthUrl: "path/to/oauth",
