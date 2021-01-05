47eb38174a854dfec583a2f6198bfb7817a09e41
Mo Sattler <hi@mosattler.com>
2021-01-05 12:44:38 +0100

    move CI to k8s

--- e2e-tests/docker-entrypoint.sh
+++ e2e-tests/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-./wait-for-it.sh -s $LOADBALANCER_HOST:$LOADBALANCER_PORT -t 60 -- echo "App is ready"
+./wait-for-it.sh -s $LOADBALANCER_HOST:$LOADBALANCER_PORT -t 300 -- echo "App is ready"
 
 exec "$@"
