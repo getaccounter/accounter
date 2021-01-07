d3b9331e60e8767bf7fd4db62565dcaf335512f0
Mo Sattler <hi@mosattler.com>
2021-01-07 09:47:06 +0100

    fix

---
+++ loadbalancer/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+envsubst '${PORT} ${PROTOCOL} ${WEB_HOST} ${WEB_PORT} ${SERVER_HOST} ${SERVER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+./wait-for-it.sh -s $SERVER_HOST:$SERVER_PORT -t 60 -- echo "Server accepts connections"
+./wait-for-it.sh -s $WEB_HOST:$WEB_PORT -t 60 -- echo "Web accepts connections"
+
+exec "$@"
