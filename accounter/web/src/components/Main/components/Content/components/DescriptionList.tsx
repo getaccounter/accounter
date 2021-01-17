55017656a9269cb1b11b30567a0642bdf5d39211
Mo Sattler <hi@mosattler.com>
2021-01-17 12:06:04 +0100

    feat(users): some UI changes in content

--- accounter/web/src/components/Main/components/Content/components/DescriptionList.tsx
+++ accounter/web/src/components/Main/components/Content/components/DescriptionList.tsx
@@ -26,28 +26,14 @@ type Props = {
 
 const DescriptionList = ({ profile }: Props) => {
   return (
-    <div className="mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-      <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-        <Description term="Email">{profile.email}</Description>
-        <Description term="Title">{profile.title ?? "-"}</Description>
-        <Description term="Team">{!profile.department ? "-" : profile.department.name}</Description>
-        <Description term="Birthday">June 8, 1990</Description>
-        <Description long term="About">
-          <p>
-            Tincidunt quam neque in cursus viverra orci, dapibus nec tristique.
-            Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus
-            congue arcu aenean posuere aliquam.
-          </p>
-          <p className="mt-5">
-            Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea
-            rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus
-            vitae. Scelerisque fermentum, cursus felis dui suspendisse velit
-            pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan
-            vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.
-          </p>
-        </Description>
-      </dl>
-    </div>
+    <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+      <Description term="Email">{profile.email}</Description>
+      <Description term="Title">{profile.title ?? "-"}</Description>
+      <Description term="Team">
+        {!profile.department ? "-" : profile.department.name}
+      </Description>
+      <Description term="Birthday">-</Description>
+    </dl>
   );
 };
 
--- accounter/web/src/components/Main/components/Content/components/Header.tsx
+++ accounter/web/src/components/Main/components/Content/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
-import { Phone } from "../../../../icons/solid";
-import { Mail } from "../../../../icons/solid";
+import { Phone, Pencil, XCircle } from "../../../../icons/solid";
 import { createFragmentContainer } from "react-relay";
 import graphql from "babel-plugin-relay/macro";
 import { Header_profile } from "./__generated__/Header_profile.graphql";
@@ -45,15 +44,15 @@ const Header = (props: Props) => {
                 type="button"
                 className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
               >
-                <Mail className="-ml-1 mr-2 h-5 w-5 text-gray-400" />
-                <span>Message</span>
+                <Pencil className="-ml-1 mr-2 h-5 w-5 text-gray-400" />
+                <span>Edit</span>
               </button>
               <button
                 type="button"
-                className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                className="inline-flex justify-center px-4 py-2 border border-red-300 shadow-sm text-sm font-medium rounded-md text-red-700 bg-white hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
               >
-                <Phone className="-ml-1 mr-2 h-5 w-5 text-gray-400" />
-                <span>Call</span>
+                <XCircle className="-ml-1 mr-2 h-5 w-5 text-red-400" />
+                <span>Offboard</span>
               </button>
             </div>
           </div>
