a28c2d8f9a38e63b8c612855432a758a90f5fb45
Mo Sattler <hi@mosattler.com>
2021-01-28 11:14:26 +0100

    test(e2e): clean up

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -29,12 +29,12 @@ import querystring from "querystring";
 
 Cypress.Commands.add(
   "register",
-  (organization, firstName, lastName, email, password) => {
-    cy.findByLabelText("Org name").type(organization);
-    cy.findByLabelText("First name").type(firstName);
-    cy.findByLabelText("Last name").type(lastName);
-    cy.findByLabelText("Email address").type(email);
-    cy.findByLabelText("Password").type(password);
+  (user) => {
+    cy.findByLabelText("Org name").type(user.organization);
+    cy.findByLabelText("First name").type(user.firstName);
+    cy.findByLabelText("Last name").type(user.lastName);
+    cy.findByLabelText("Email address").type(user.email);
+    cy.findByLabelText("Password").type(user.password);
     cy.findByRole("button", { name: "Sign up" }).click();
   }
 );
