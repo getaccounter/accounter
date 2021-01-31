fe9aa520957fd469313ce33b659f2d4ad61ac26f
Mo Sattler <hi@mosattler.com>
2021-01-31 21:30:30 +0100

    fix: deployment

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@ doctl registry login
 docker tag server.production $IMAGE_REGISTRY/server:$@
 docker tag web.production $IMAGE_REGISTRY/web:$@
 docker tag loadbalancer $IMAGE_REGISTRY/loadbalancer:$@
+docker tag connector.development $IMAGE_REGISTRY/connector:$@
 docker push $IMAGE_REGISTRY/server:$@
 docker push $IMAGE_REGISTRY/web:$@
 docker push $IMAGE_REGISTRY/loadbalancer:$@
+docker push $IMAGE_REGISTRY/connector:$@
 
 export TF_VAR_image_tag=$@
 terraform -chdir=provider apply -auto-approve
