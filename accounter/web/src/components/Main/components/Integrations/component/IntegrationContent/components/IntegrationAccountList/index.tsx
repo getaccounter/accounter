96a3a72e6287c1699c4f4f3e31f4756db7f80514
Mo Sattler <hi@mosattler.com>
2021-02-03 21:50:11 +0100

    feat(integrations): implement new account design

--- accounter/web/src/components/Main/components/Integrations/component/IntegrationContent/components/IntegrationAccountList/index.tsx
+++ accounter/web/src/components/Main/components/Integrations/component/IntegrationContent/components/IntegrationAccountList/index.tsx
@@ -1,7 +1,8 @@
 import { createFragmentContainer } from "react-relay";
-import IntegrationAccount from "./components/IntegrationAccount";
 import graphql from "babel-plugin-relay/macro";
 import { IntegrationAccountList_accounts } from "./__generated__/IntegrationAccountList_accounts.graphql";
+import React from "react";
+import Table from "../../../../../../../Table";
 
 type Props = {
   accounts: IntegrationAccountList_accounts;
@@ -9,14 +10,73 @@ type Props = {
 
 const IntegrationAccountList = ({ accounts }: Props) => {
   return (
-    <div>
-      {/* <h2 className="text-sm font-medium text-gray-500">Accounts</h2> */}
-      <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {accounts.map((account) => (
-          <IntegrationAccount key={account.id} account={account} />
-        ))}
-      </div>
-    </div>
+    <>
+      <Table
+        title="Accounts"
+        head={[
+          {
+            value: "name",
+            label: "Name",
+          },
+          {
+            value: "username",
+            label: "Username",
+          },
+          {
+            value: "role",
+            label: "Role",
+          },
+          {
+            value: "edit",
+            label: "Edit",
+            hidden: true,
+          },
+        ]}
+        entries={accounts.map((account) => {
+          const fullName = `${account.profile.firstName} ${account.profile.lastName}`
+          return ({
+            name: (
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0 h-10 w-10">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={account.image}
+                      alt={fullName}
+                    />
+                  </div>
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-gray-900">
+                      {fullName}
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      {account.profile.title}
+                    </div>
+                  </div>
+                </div>
+              </td>
+            ),
+            username: (
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {account.username}
+              </td>
+            ),
+            role: (
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {account.role}
+              </td>
+            ),
+            edit: (
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a href="#asd" className="text-indigo-600 hover:text-indigo-900">
+                  Edit
+                </a>
+              </td>
+            ),
+          })
+        })}
+      />
+    </>
   );
 };
 
@@ -25,7 +85,14 @@ export default createFragmentContainer(IntegrationAccountList, {
     fragment IntegrationAccountList_accounts on AccountNode
     @relay(plural: true) {
       id
-      ...IntegrationAccount_account
+      image
+      profile {
+        firstName
+        lastName
+        title
+      }
+      username
+      role
     }
   `,
 });
