8f28f74b8f89858d1128a0a89f92a85b4174bb5c
Moritz Sattler <hi@mosattler.com>
2021-02-07 00:22:45 +0100

    chore: dc fix

--- accounter/web/dev-setup.sh
+++ accounter/web/dev-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# I dont know why, but this has to be done for CRA to work
+chmod -R 0777 node_modules/
+
 # Temporary, until heroicons library is stable and transpiled
 # The copying is already done in the dockerfile, but here we do it again
 # since the folder will be overwritten with volumes
