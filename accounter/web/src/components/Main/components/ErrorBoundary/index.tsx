e9485d545b1011282c17300f27e71e74cfc6a4a5
Moritz Sattler <hi@mosattler.com>
2021-02-13 17:46:52 +0100

    feat(web): add sentry
    closes #525

--- accounter/web/src/components/Main/components/ErrorBoundary/index.tsx
+++ accounter/web/src/components/Main/components/ErrorBoundary/index.tsx
@@ -1,40 +1,34 @@
-import React, { Component, ErrorInfo, ReactNode } from "react";
+import React, { ReactNode } from "react";
+import * as Sentry from "@sentry/react";
 
 interface Props {
   children: ReactNode;
 }
 
-interface State {
-  hasError: boolean;
-}
-
-class ErrorBoundary extends Component<Props, State> {
-  public state: State = {
-    hasError: false,
-  };
-
-  public static getDerivedStateFromError(_: Error): State {
-    return { hasError: true };
-  }
-
-  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Uncaught error:", error, errorInfo);
-  }
-
-  public render() {
-    if (this.state.hasError) {
-      return (
-        <div className="flex justify-center items-center height h-full">
+const ErrorBoundary = ({ children }: Props) => (
+  <Sentry.ErrorBoundary
+    fallback={
+      <div className="flex flex-col items-center ">
+        <div className="flex justify-center height h-full">
           <h1>
             Something went wrong. Please try again. If the issue still persists,
             please contact us.
           </h1>
         </div>
-      );
+        <a href="/">
+          <button
+            type="button"
+            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Go back
+          </button>
+        </a>
+      </div>
     }
-
-    return this.props.children;
-  }
-}
+    showDialog
+  >
+    {children}
+  </Sentry.ErrorBoundary>
+);
 
 export default ErrorBoundary;
