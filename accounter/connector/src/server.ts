acc0b7199433ce36955fdf3e742f81c7f27816ca
Moritz Sattler <hi@mosattler.com>
2021-02-13 12:49:27 +0100

    feat(connector): add logger

--- accounter/connector/src/server.ts
+++ accounter/connector/src/server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import { NODE_ENV } from "./env";
 import googleApp from "./google";
 import slackApp from "./slack";
+import logger from 'morgan'
 
 if (NODE_ENV === "development") {
   // quite hacky, I dont like it, but nodejs doesnt seem to pick up the
@@ -10,6 +11,9 @@ if (NODE_ENV === "development") {
 }
 
 const server = express()
+
+server.use(logger(NODE_ENV === "development" ? "dev" : 'common'))
+
 server.use("/google", googleApp)
 server.use("/slack", slackApp)
 
