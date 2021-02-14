11d47886f78f1bd43b29c16c91d45071549f31f9
Moritz Sattler <hi@mosattler.com>
2021-02-14 14:16:40 +0100

    feat: tweak a bit branding

--- accounter/web/src/components/Main/components/Sidebar/index.tsx
+++ accounter/web/src/components/Main/components/Sidebar/index.tsx
@@ -108,11 +108,13 @@ const DesktopSidebar = (props: Props) => (
         <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-gray-100">
           <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-pink-500-mark-gray-900-text.svg"
-                alt="Workflow"
-              />
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <span className="text-xl font-semibold pl-2">Accounter</span>
+            
             </div>
             <nav className="mt-5 flex-1" aria-label="Sidebar">
               <div className="px-2 space-y-1">
