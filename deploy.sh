7eb508ec833683804b0174296a4b787ff2776f97
Moritz Sattler <hi@mosattler.com>
2021-02-14 16:14:16 +0100

    fix(connector): run prod version

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ doctl registry login
 docker tag server.production $IMAGE_REGISTRY/server:$@
 docker tag web.production $IMAGE_REGISTRY/web:$@
 docker tag loadbalancer $IMAGE_REGISTRY/loadbalancer:$@
-docker tag connector.development $IMAGE_REGISTRY/connector:$@
+docker tag connector.production $IMAGE_REGISTRY/connector:$@
 docker push $IMAGE_REGISTRY/server:$@
 docker push $IMAGE_REGISTRY/web:$@
 docker push $IMAGE_REGISTRY/loadbalancer:$@
